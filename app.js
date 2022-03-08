ce01475d52e6137a2aad700b2beb051f05715254
omer <omeratayilmaz@gmail.com>
2022-03-08 20:22:05 +0300

    m

--- app.js
+++ app.js
@@ -16,91 +16,94 @@ const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
+const viewRouter = require("./routes/viewRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
 const bookingController = require("./controllers/bookingController");
-const viewRouter = require("./routes/viewRoutes");
 
-// Start express app
+//GLOBAL MIDDLEWARES
 const app = express();
 
 app.enable("trust proxy");
-
+//defining view engine
 app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views")); //views adındaki klasörün yolunu getirir.
 
-// 1) GLOBAL MIDDLEWARES
-// Implement CORS
-app.use(cors());
-// Access-Control-Allow-Origin *
-// api.natours.com, front-end natours.com
-// app.use(cors({
-//   origin: 'https://www.natours.com'
-// }))
-
-app.options("*", cors());
-// app.options('/api/v1/tours/:id', cors());
-
-// Serving static files
+app.use(cors()); //only post and get. extra: corsa {origin:frontendUrl} yaparak requestleri sadece belirli bir yerden isteyebiliriz.
+app.options("*", cors()); //put-patch-delete-update
+//serving static files
 app.use(express.static(path.join(__dirname, "public")));
-
-// Set security HTTP headers
-app.use(helmet());
-
-// Development logging
+//Set Security HTTP headers
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    contentSecurityPolicy: false,
+  })
+);
+//Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
-// Limit requests from same API
+//Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
+  message: "Too many request from this IP, please try again in an hour!",
 });
+//sadece icide /api gecenlerde calisir. :)
 app.use("/api", limiter);
-
-// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+//express.json()'dan önce olmalıdır!
 app.post(
   "/webhook-checkout",
   bodyParser.raw({ type: "application/json" }),
   bookingController.webhookCheckout
 );
 
-// Body parser, reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+//body parser,reading data from the body into req.body
+app.use(
+  express.json({
+    limit: "10kb", //10kb gecen veri kabul edilmez
+  })
+);
+//post requestten dala almak icin kullandık, yoksa bos obje donuyor
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "10kb",
+  })
+);
 app.use(cookieParser());
-
-// Data sanitization against NoSQL query injection
-app.use(mongoSanitize());
-
-// Data sanitization against XSS
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize()); //req.body,req.params etc. gibi yerlere bakar ve $ işareti gibi injecion yapılabilecek data var mı kontrol eder.
+//Data sanitization against XSS
 app.use(xss());
+//clean any malicious html code
+//"&lt;h4>name&lt;/h4>", html taglarini buna cevirdi
 
-// Prevent parameter pollution
+//prevent parameter pollution
 app.use(
   hpp({
     whitelist: [
       "duration",
-      "ratingsQuantity",
-      "ratingsAverage",
       "maxGroupSize",
+      "ratingsAverage",
+      "ratingsQuantity",
       "difficulty",
       "price",
     ],
   })
 );
+//or:{{URL}}api/v1/tours?sort=duration&sort=price hem duration hem de price gore sıralamaz en sondakini alır- yazmasak error verir-
 
 app.use(compression());
-
-// Test middleware
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  // console.log(req.cookies);
+  //(req.cookies);
   next();
 });
+//serving static filesçc
+//app.use(express.static(`${__dirname}/public`));
 
-// 3) ROUTES
 app.use("/", viewRouter);
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
@@ -110,7 +113,5 @@ app.use("/api/v1/bookings", bookingRouter);
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
-
 app.use(globalErrorHandler);
-
 module.exports = app;
