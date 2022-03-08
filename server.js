b3d7239c17932490c8360cb934a0a33234aa89ca
omer <omeratayilmaz@gmail.com>
2022-03-08 19:48:38 +0300

    m

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ const server = app.listen(port, () => {
 process.on("unhandledRejection", (err) => {
   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
   console.log(err.name, err.message);
+  console.log(err.stack);
   server.close(() => {
     process.exit(1);
   });
